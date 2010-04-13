c5c49d5f6ac5bac18e40585fb26d58e90b62cfbc
Peter Berkenbosch <peter@pero-ict.nl>
2010-04-13 22:27:40 +0200

    override het checkouts helper.

--- mollie_ideal_extension.rb
+++ mollie_ideal_extension.rb
@@ -13,9 +13,9 @@ class MollieIdealExtension < Spree::Extension
   
   def activate
     # make your helper avaliable in all views
-    Spree::BaseController.class_eval do
-      helper MollieIdealHelper
-    end
+    #Spree::BaseController.class_eval do
+    #  helper MollieIdealHelper
+    #end
     
     BillingIntegration::MollieIdeal.register
     Checkout.class_eval do
@@ -32,6 +32,33 @@ class MollieIdealExtension < Spree::Extension
           transition :to => 'complete', :from => 'payment'
         end
       end
+    end
+    
+    CheckoutsHelper.class_eval do
+      def checkout_progress
+        steps = Checkout.state_names.reject { |n| n == "complete" }.map do |state|
+          text = t("checkout_steps.#{state}")
+
+          css_classes = []
+          current_index = Checkout.state_names.index(@checkout.state)
+          state_index = Checkout.state_names.index(state)
+
+          if state_index < current_index
+            css_classes << 'completed'
+            text = link_to text, edit_order_checkout_url(@order, :step => state)
+          end
+
+          css_classes << 'next' if state_index == current_index + 1
+          css_classes << 'current' if state == @checkout.state
+          css_classes << 'first' if state_index == 0
+          css_classes << 'last' if state_index == Checkout.state_names.length - 1
+
+          # It'd be nice to have separate classes but combining them with a dash helps out for IE6 which only sees the last class
+          content_tag('li', content_tag('span', text), :class => css_classes.join('-'))
+        end
+        content_tag('ol', steps.join("\n"), :class => 'progress-steps', :id => "checkout-step-#{@checkout.state}") + '<br clear="left" />'
+      end
+    
     end
     
     #open up CheckoutsController so I can do a proper redirection.
