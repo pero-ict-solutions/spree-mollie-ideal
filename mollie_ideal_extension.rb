64d584850b8f9d94ef1f87049bda2293cf0e1555
Peter Berkenbosch <peter@pero-ict.nl>
2010-04-13 23:30:17 +0200

    fixing with adding payment_profiles override. This needs some extra looking into. Since Spree is based on only 1 Gateway.

--- mollie_ideal_extension.rb
+++ mollie_ideal_extension.rb
@@ -18,47 +18,10 @@ class MollieIdealExtension < Spree::Extension
     #end
     
     BillingIntegration::MollieIdeal.register
-    Checkout.class_eval do
-    
-      # checkout state machine (see http://github.com/pluginaweek/state_machine/tree/master for details)
-      state_machine :initial => 'address' do
-        after_transition :to => 'complete', :do => :complete_order
-        before_transition :to => 'complete', :do => :process_payment
-        event :next do
-          transition :to => 'delivery', :from  => 'address'
-          transition :to => 'payment', :from => 'delivery'
-          transition :to => 'confirm', :from => 'payment'
-          transition :to => 'complete', :from => 'confirm'
-          transition :to => 'complete', :from => 'payment'
-        end
+    PaymentMethod.class_eval do
+      def payment_profiles_supported?
+        true
       end
-    end
-    
-    CheckoutsHelper.class_eval do
-      def checkout_progress
-        steps = Checkout.state_names.reject { |n| n == "complete" }.map do |state|
-          text = t("checkout_steps.#{state}")
-
-          css_classes = []
-          current_index = Checkout.state_names.index(@checkout.state)
-          state_index = Checkout.state_names.index(state)
-
-          if state_index < current_index
-            css_classes << 'completed'
-            text = link_to text, edit_order_checkout_url(@order, :step => state)
-          end
-
-          css_classes << 'next' if state_index == current_index + 1
-          css_classes << 'current' if state == @checkout.state
-          css_classes << 'first' if state_index == 0
-          css_classes << 'last' if state_index == Checkout.state_names.length - 1
-
-          # It'd be nice to have separate classes but combining them with a dash helps out for IE6 which only sees the last class
-          content_tag('li', content_tag('span', text), :class => css_classes.join('-'))
-        end
-        content_tag('ol', steps.join("\n"), :class => 'progress-steps', :id => "checkout-step-#{@checkout.state}") + '<br clear="left" />'
-      end
-    
     end
     
     #open up CheckoutsController so I can do a proper redirection.
