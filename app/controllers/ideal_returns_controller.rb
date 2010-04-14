f75c80c228507e26c5d6153c736ba7471dfc8987
Peter Berkenbosch <peter@pero-ict.nl>
2010-04-14 20:56:58 +0200

    added message with failing iDEAL.

--- app/controllers/ideal_returns_controller.rb
+++ app/controllers/ideal_returns_controller.rb
@@ -6,11 +6,11 @@ class IdealReturnsController < ApplicationController
     payment = IdealPayment.find_by_transaction_id(transaction_id)
     if payment.is_payed?
       payment.payments.first.finalize!
-      session[:order_id] = nil
       flash[:commerce_tracking] = I18n.t("notice_messages.track_me_in_GA")
       redirect_to order_url(payment.payments.first.order, {:checkout_complete => true, :order_token => payment.payments.first.order.token}) 
     else
-      flash[:notice] = "Kies aub een andere betaalmethode. iDEAL betaling is niet gelukt of afgebroken."
+      flash[:notice] = "iDEAL betaling is niet gelukt of afgebroken. U kunt de bestelling via een bankoverschrijving voldoen."
+      session[:order_id] = nil
       redirect_to order_url(payment.payments.first.order)
     end
   end
