e1df22e120e3553b79fcea7b566e07114c807d3e
Peter Berkenbosch <peter@pero-ict.nl>
2010-04-14 08:20:14 +0200

    removed call to prev.

--- app/controllers/ideal_returns_controller.rb
+++ app/controllers/ideal_returns_controller.rb
@@ -10,7 +10,6 @@ class IdealReturnsController < ApplicationController
       flash[:commerce_tracking] = I18n.t("notice_messages.track_me_in_GA")
       redirect_to order_url(payment.payments.first.order, {:checkout_complete => true, :order_token => payment.payments.first.order.token}) 
     else
-      payment.payments.first.order.checkout.prev!
       flash[:notice] = "Kies aub een andere betaalmethode. iDEAL betaling is niet gelukt of afgebroken."
       redirect_to order_url(payment.payments.first.order)
     end
