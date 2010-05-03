8b3cd50634ce30a3365f9f2d9d9b5f6f8a964d82
Peter Berkenbosch <peter@pero-ict.nl>
2010-05-03 22:15:03 +0200

    added extra check on order.status == "paid" since mollie API returns a false status for is_payed when requested for the second time. MOLLIE SUPPORT IS BEING MAILED!

--- app/controllers/ideal_returns_controller.rb
+++ app/controllers/ideal_returns_controller.rb
@@ -4,14 +4,20 @@ class IdealReturnsController < ApplicationController
     Rails.logger.info "[IDEAL] GET return received from mollie"
     transaction_id = params[:transaction_id]
     payment = IdealPayment.find_by_transaction_id(transaction_id)
-    if payment.is_payed?
-      payment.payments.first.finalize!
+    order = payment.payments.first.order
+    if order.state == "paid"
       flash[:commerce_tracking] = I18n.t("notice_messages.track_me_in_GA")
-      redirect_to order_url(payment.payments.first.order, {:checkout_complete => true, :order_token => payment.payments.first.order.token}) 
+      redirect_to order_url(payment.payments.first.order, {:checkout_complete => true, :order_token => order.token})
     else
-      flash[:notice] = "iDEAL betaling is niet gelukt of afgebroken. U kunt de bestelling via een bankoverschrijving voldoen."
-      session[:order_id] = nil
-      redirect_to order_url(payment.payments.first.order)
+      if payment.is_payed?
+        payment.payments.first.finalize!
+        flash[:commerce_tracking] = I18n.t("notice_messages.track_me_in_GA")
+        redirect_to order_url(payment.payments.first.order, {:checkout_complete => true, :order_token => order.token})
+      else
+        flash[:notice] = "iDEAL betaling is niet gelukt of afgebroken. U kunt de bestelling via een bankoverschrijving voldoen."
+        session[:order_id] = nil
+        redirect_to order_url(payment.payments.first.order)
+      end
     end
   end
   
