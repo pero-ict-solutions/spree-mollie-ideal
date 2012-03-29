7c72049ec89bbf64dcf08a6e3f96a2df70e9cc85
Johan Bruning <johan.h.bruning@gmail.com>
2012-03-29 20:48:51 +0200

    Moved to namespace

---
+++ app/models/spree/billing_integration/mollie_ideal.rb
@@ -0,0 +1,20 @@
+module Spree
+  class BillingIntegration::MollieIdeal < BillingIntegration
+    preference :partner_id, :string
+    preference :hostname, :string
+    preference :callback_url, :string
+
+    def banklist
+      Mollie::Ideal.testmode = IdealPayment.current_payment_method.preferred_test_mode
+      Mollie::Ideal.banklist
+    end
+
+    def provider_class
+      #ActiveMerchant::Billing::MollieIdealGateway
+    end
+
+    def payment_source_class
+      IdealPayment
+    end
+  end
+end
