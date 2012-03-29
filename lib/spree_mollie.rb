3213e64d378646eee3810d2e18217a1bc533f614
Johan Bruning <johan.h.bruning@gmail.com>
2012-03-29 22:40:44 +0200

    Remove lines that didn't belong there. They are already in spree-mollie/engine

--- lib/spree_mollie.rb
+++ lib/spree_mollie.rb
@@ -1,6 +1,2 @@
 require 'spree_core'
 require 'spree_mollie/engine'
-
-Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
-  Rails.configuration.cache_classes ? require(c) : load(c)
-end
