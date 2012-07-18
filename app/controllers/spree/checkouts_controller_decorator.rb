25ceb148ecd1bfa83d915a401f399b7a1282fd47
Johan Bruning <johan.h.bruning@gmail.com>
2012-07-18 22:35:22 +0200

    'semi'-working
    For details see: https://groups.google.com/forum/?fromgroups#!topic/spree-user/JlMGUwulGY4

--- app/controllers/spree/checkouts_controller_decorator.rb
+++ app/controllers/spree/checkouts_controller_decorator.rb
@@ -1,15 +1,26 @@
 module Spree
   CheckoutController.class_eval do
-    def complete_checkout
-      complete_order
-      order_params = {:checkout_complete => true}
-      if @checkout.payments.first.source_type == "IdealPayment"
-        redirect_to @checkout.payments.first.source.payment_redirect_url
+    def completion_route
+      if @order.payments.first.source_type == "Spree::IdealPayment"
+        @order.payments.first.source.payment_redirect_url
       else
-        session[:order_id] = nil
-        flash[:commerce_tracking] = I18n.t("notice_messages.track_me_in_GA")
-        redirect_to order_url(@order, {:checkout_complete => true, :order_token => @order.token})
+        order_path(@order)
       end
+    end
+    def before_complete
+      logger.info "\n \n \n \n \n \n I'm In Before Complete!!!!! #{@order.state}  \n \n \n \n \n"
+    end
+
+    def before_confirm
+      logger.info "\n \n \n \n \n \n I'm In Before Confirm!!!!! #{@order.state}  \n \n \n \n \n"
+    end
+
+    def after_confirm
+      logger.info "\n \n \n \n \n \n I'm In After Confirm!!!!! #{@order.state}  \n \n \n \n \n"
+    end
+
+    def after_payment
+      logger.info "\n \n \n \n \n \n I'm In After Payment!!!!! #{@order.state} \n \n \n \n \n"
     end
   end
 end
