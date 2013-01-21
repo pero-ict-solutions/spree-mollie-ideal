cda0f0c5286dedb80d6fdce60dbc7e06b5f8cd3d
Johan Bruning <johan@wirelab.nl>
2013-01-21 12:01:36 +0100

    Rename migration to avoid duplicates

--- db/migrate/20120329185541_prefix_mollie_table_names.rb
+++ db/migrate/20120329185541_prefix_mollie_table_names.rb
@@ -1,4 +1,4 @@
-class PrefixTableNames < ActiveRecord::Migration
+class PrefixMollieTableNames < ActiveRecord::Migration
   def change
     rename_table :ideal_payments, :spree_ideal_payments
   end
